df4381680f797989fc067bf2b7e427955bfe86b1
laava <laavart11@gmail.com>
2022-02-22 10:50:24 +0530

    Commit

--- src/main/java/com/psloba/megabucks/LogInApplication.java
+++ src/main/java/com/psloba/megabucks/LogInApplication.java
@@ -1,11 +1,15 @@
 package com.psloba.megabucks;
 
+import com.psloba.citra.Client;
 import com.psloba.citra.Database;
 import com.psloba.citra.Source;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LogInApplication extends Application {
@@ -18,8 +22,19 @@ public class LogInApplication extends Application {
         stage.setResizable(false);
         stage.show();
 
-        Database db = Database.connect(Source.MYSQL, "localhost", "megabucks", "root", "1234");
+        Database database = Database.connect(Source.MYSQL, "localhost", "megabucks", "root", "1234");
 
+        Button login = (Button) fxmlLoader.getNamespace().get("login");
+        login.isDefaultButton();
+
+        TextField username = (TextField) fxmlLoader.getNamespace().get("username");
+        PasswordField password = (PasswordField) fxmlLoader.getNamespace().get("password");
+
+        assert database != null;
+        Client client = database.validateUser(username.getText(), password.getText());
+
+        if(client != null) System.out.println("Working");
+        else System.out.println("Not Working");
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/psloba/megabucks/LogInController.java
@@ -0,0 +1,16 @@
+package com.psloba.megabucks;
+
+import javafx.fxml.FXML;
+
+public class LogInController {
+
+    @FXML
+    private void onClickingForgot(){
+
+    }
+
+    @FXML
+    private void onClickingCreate(){
+
+    }
+}
