19917f9a954a3c601a994847e932ced04c88e463
laava <laavart11@gmail.com>
2022-02-27 02:04:11 +0530

    Commit

--- src/main/java/com/psloba/megabucks/Create.java
+++ src/main/java/com/psloba/megabucks/Create.java
@@ -2,8 +2,23 @@ package com.psloba.megabucks;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 
 public class Create {
+
+    public TextField name;
+    public DatePicker dob;
+    public TextField email;
+    public TextField mobile;
+    public PasswordField password;
+    public TextField username;
+    public PasswordField repassword;
+    public HBox message;
+    public TextField address1;
+    public TextField address2;
+    public TextField postal;
+    public TextField city;
+    public TextField state;
 
     @FXML
     private void onBack(){
