de71ce6b168413e3d04ae4c41bb88e6392a774c2
laava <laavart11@gmail.com>
2022-02-27 03:09:18 +0530

    Commit

--- src/main/java/com/psloba/megabucks/Create.java
+++ src/main/java/com/psloba/megabucks/Create.java
@@ -1,16 +1,16 @@
 package com.psloba.megabucks;
 
 import com.psloba.citra.client.*;
-import com.psloba.citra.Client;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.paint.Color;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class Create {
 
+    public TextArea messagebox;
     public TextField name;
     public DatePicker dob;
     public TextField email;
@@ -18,45 +18,113 @@ public class Create {
     public PasswordField password;
     public TextField username;
     public PasswordField repassword;
-    public Label message;
+    public TextField passcode;
     public TextField address1;
     public TextField address2;
     public TextField postal;
     public TextField city;
     public TextField state;
+    public TextField country;
 
     @FXML
     private void checkingEmail(){
+        String message = "E-mail ID Invalid!\n";
         if(!Pattern.compile("^(?=.{7,150})[a-zA-Z0-9+._-]+@[a-zA-Z0-9.]+$").matcher(email.getText()).matches())
-            message.setText("E-mail ID Invalid!");
+            messagebox.appendText(message);
+        else if(messagebox.getText().contains(message)) {
+            int i = messagebox.getText().indexOf(message);
+            int l = messagebox.getText().length();
+            messagebox.deleteText(i, i+l);
+        }
     }
 
     @FXML
     private void checkingMobile(){
+        String message = "Mobile No. Invalid!\n";
         if(mobile.getText().length() != 10)
-            message.setText("Mobile No. Invalid!");
+            messagebox.appendText(message);
+        else if(messagebox.getText().contains(message)) {
+            int i = messagebox.getText().indexOf(message);
+            int l = messagebox.getText().length();
+            messagebox.deleteText(i, i+l);
+        }
     }
 
     @FXML
     private void settingUsername() {
+        String message = "Username Unavailable!\n";
         if(Application.database.searchUser(username.getText()))
-            message.setText("Username Unavailable!");
+            messagebox.appendText(message);
+        else if(messagebox.getText().contains(message)) {
+            int i = messagebox.getText().indexOf(message);
+            int l = messagebox.getText().length();
+            messagebox.deleteText(i, i+l);
+        }
     }
 
     @FXML
     private void settingPassword(){
+        String message = "Password Invalid!\n";
         if(!Pattern.compile("^.*(?=.{8,128})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$").matcher(password.getText()).matches())
-            System.out.println("Password Invalid!");
+            messagebox.appendText(message);
+        else if(messagebox.getText().contains(message)) {
+            int i = messagebox.getText().indexOf(message);
+            int l = messagebox.getText().length();
+            messagebox.deleteText(i, i+l);
+        }
     }
 
     @FXML
     private void matchingPassword(){
-        if(username.getText() != repassword.getText())
-            System.out.println("Password Mismatch!");
+        String message = "Password Mismatch!\n";
+        if(!Objects.equals(username.getText(), repassword.getText()))
+            messagebox.appendText(message);
+        else if(messagebox.getText().contains(message)) {
+            int i = messagebox.getText().indexOf(message);
+            int l = messagebox.getText().length();
+            messagebox.deleteText(i, i+l);
+        }
+    }
+
+    @FXML
+    private void settingPassCode(){
+        String message = "PassCode Invalid!\n";
+        if(passcode.getText().length() != 7)
+            messagebox.appendText(message);
+        else if(messagebox.getText().contains(message)) {
+            int i = messagebox.getText().indexOf(message);
+            int l = messagebox.getText().length();
+            messagebox.deleteText(i, i+l);
+        }
     }
 
     @FXML
     private void onBack(){
         Application.stage.setScene(Application.Scenes.get("login.fxml"));
+    }
+
+    @FXML
+    private void onClear(){
+        name.setText("");
+        dob.setValue(null);
+        email.setText("");
+        mobile.setText("");
+        username.setText("");
+        password.setText("");
+        repassword.setText("");
+        address1.setText("");
+        address2.setText("");
+        postal.setText("");
+        city.setText("");
+        state.setText("");
+        country.setText("");
+    }
+
+    @FXML
+    private void onSubmit() {
+        User user = new User(username.getText(), name.getText(), dob.getValue());
+        Comm comm = new Comm(email.getText(), mobile.getText());
+        Security security = new Security(password.getText(), passcode.getText());
+        Address address = new Address(address1.getText(), address2.getText(), postal.getText(), city.getText(), state.getText(), country.getText());
     }
 }
