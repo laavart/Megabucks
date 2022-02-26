2cbb17eff9b1944831d931c5268166fc19b28a90
laava <laavart11@gmail.com>
2022-02-27 02:33:18 +0530

    Commit

--- src/main/java/com/psloba/megabucks/Create.java
+++ src/main/java/com/psloba/megabucks/Create.java
@@ -1,12 +1,13 @@
 package com.psloba.megabucks;
 
-import com.psloba.citra.Database;
 import com.psloba.citra.client.*;
 import com.psloba.citra.Client;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+
+import java.util.regex.Pattern;
 
 public class Create {
 
@@ -25,13 +26,37 @@ public class Create {
     public TextField state;
 
     @FXML
-    private void onBack(){
-        Application.stage.setScene(Application.Scenes.get("login.fxml"));
+    private void checkingEmail(){
+        if(!Pattern.compile("^(?=.{7,150})[a-zA-Z0-9+._-]+@[a-zA-Z0-9.]+$").matcher(email.getText()).matches())
+            message.setText("E-mail ID Invalid!");
+    }
+
+    @FXML
+    private void checkingMobile(){
+        if(mobile.getText().length() != 10)
+            message.setText("Mobile No. Invalid!");
     }
 
     @FXML
     private void settingUsername() {
-        if(Application.database.searchUser(username.getText())) message.setText("");
+        if(Application.database.searchUser(username.getText()))
+            message.setText("Username Unavailable!");
     }
 
+    @FXML
+    private void settingPassword(){
+        if(!Pattern.compile("^.*(?=.{8,128})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$").matcher(password.getText()).matches())
+            System.out.println("Password Invalid!");
+    }
+
+    @FXML
+    private void matchingPassword(){
+        if(username.getText() != repassword.getText())
+            System.out.println("Password Mismatch!");
+    }
+
+    @FXML
+    private void onBack(){
+        Application.stage.setScene(Application.Scenes.get("login.fxml"));
+    }
 }
