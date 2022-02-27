ad70736ddbc9649af08539a74e233bebdbf90946
laava <laavart11@gmail.com>
2022-02-27 10:53:39 +0530

    Commit

--- src/main/java/com/psloba/megabucks/Create.java
+++ src/main/java/com/psloba/megabucks/Create.java
@@ -32,11 +32,8 @@ public class Create {
         String message = "E-mail ID Invalid!\n";
         if(!Pattern.compile("^(?=.{7,150})[a-zA-Z0-9+._-]+@[a-zA-Z0-9.]+$").matcher(email.getText()).matches())
             messagebox.appendText(message);
-        else while(messagebox.getText().contains(message)) {
-            int i = messagebox.getText().indexOf(message);
-            int l = messagebox.getText().length();
-            messagebox.deleteText(i,l);
-        }
+        else while(messagebox.getText().contains(message))
+            messagebox.deleteText(messagebox.getText().indexOf(message), messagebox.getText().length());
     }
 
     @FXML
@@ -44,11 +41,8 @@ public class Create {
         String message = "Mobile No. Invalid!\n";
         if(mobile.getText().length() != 10)
             messagebox.appendText(message);
-        else while(messagebox.getText().contains(message)) {
-            int i = messagebox.getText().indexOf(message);
-            int l = messagebox.getText().length();
-            messagebox.deleteText(i,l);
-        }
+        else while(messagebox.getText().contains(message))
+            messagebox.deleteText(messagebox.getText().indexOf(message), messagebox.getText().length());
     }
 
     @FXML
@@ -56,11 +50,8 @@ public class Create {
         String message = "Username Unavailable!\n";
         if(Application.database.searchUser(username.getText()))
             messagebox.appendText(message);
-        else while(messagebox.getText().contains(message)) {
-            int i = messagebox.getText().indexOf(message);
-            int l = messagebox.getText().length();
-            messagebox.deleteText(i,l);
-        }
+        else while(messagebox.getText().contains(message))
+            messagebox.deleteText(messagebox.getText().indexOf(message), messagebox.getText().length());
     }
 
     @FXML
@@ -68,11 +59,8 @@ public class Create {
         String message = "Password Invalid!\n";
         if(!Pattern.compile("^.*(?=.{8,128})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$").matcher(password.getText()).matches())
             messagebox.appendText(message);
-        else while(messagebox.getText().contains(message)) {
-            int i = messagebox.getText().indexOf(message);
-            int l = messagebox.getText().length();
-            messagebox.deleteText(i,l);
-        }
+        else while(messagebox.getText().contains(message))
+            messagebox.deleteText(messagebox.getText().indexOf(message), messagebox.getText().length());
     }
 
     @FXML
@@ -80,11 +68,8 @@ public class Create {
         String message = "Password Mismatch!\n";
         if(!Objects.equals(password.getText(), repassword.getText()))
             messagebox.appendText(message);
-        else while(messagebox.getText().contains(message)) {
-            int i = messagebox.getText().indexOf(message);
-            int l = messagebox.getText().length();
-            messagebox.deleteText(i,l);
-        }
+        else while(messagebox.getText().contains(message))
+            messagebox.deleteText(messagebox.getText().indexOf(message), messagebox.getText().length());
     }
 
     @FXML
@@ -92,11 +77,8 @@ public class Create {
         String message = "PassCode Invalid!\n";
         if(passcode.getText().length() != 7)
             messagebox.appendText(message);
-        else while(messagebox.getText().contains(message)) {
-            int i = messagebox.getText().indexOf(message);
-            int l = messagebox.getText().length();
-            messagebox.deleteText(i,l);
-        }
+        else while(messagebox.getText().contains(message))
+            messagebox.deleteText(messagebox.getText().indexOf(message), messagebox.getText().length());
     }
 
     @FXML
@@ -111,11 +93,8 @@ public class Create {
                 state.setText(address.state());
                 country.setText(address.country());
             }
-            while (messagebox.getText().contains(message)) {
-                int i = messagebox.getText().indexOf(message);
-                int l = messagebox.getText().length();
-                messagebox.deleteText(i, l);
-            }
+            while(messagebox.getText().contains(message))
+                messagebox.deleteText(messagebox.getText().indexOf(message), messagebox.getText().length());
         }
     }
 
@@ -143,22 +122,41 @@ public class Create {
 
     @FXML
     private void onSubmit() {
-        User user = new User(username.getText(), name.getText(), dob.getValue());
-        Comm comm = new Comm(email.getText(), mobile.getText());
-        Security security = new Security(password.getText(), passcode.getText());
-        Address address = new Address(address1.getText(), address2.getText(), postal.getText(), city.getText(), state.getText(), country.getText());
-        Application.client = new Client(user, security, comm, address);
-        int id = Application.database.addNewUser(Application.client);
-        Alert alert;
-        if(id != -1) {
-            alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setTitle("Accounts!");
-            alert.setContentText("Account Created!");
-        } else {
-            alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Accounts!");
-            alert.setContentText("Unable to Create Account!");
+        if(username.getText()==null) username.setText("FIELD EMPTY!");
+        else if(email.getText()==null) email.setText("FIELD EMPTY!");
+        else if(mobile.getText()==null) mobile.setText("FIELD EMPTY!");
+        else if(password.getText()==null) password.setText("FIELD EMPTY!");
+        else if(repassword.getText()==null) repassword.setText("FIELD EMPTY!");
+        else if(passcode.getText()==null) passcode.setText("FIELD EMPTY!");
+        else if(address1.getText()==null) address1.setText("FIELD EMPTY!");
+        else if(postal.getText()==null) postal.setText("FIELD EMPTY!");
+        else if(city.getText()==null) city.setText("FIELD EMPTY!");
+        else if(state.getText()==null) state.setText("FIELD EMPTY!");
+        else if(country.getText()==null) country.setText("FIELD EMPTY!");
+        else if(!messagebox.getText().equals("Messages:")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Form!");
+            alert.setContentText("Resolve all the Problems First!");
+            alert.show();
         }
-        alert.show();
+        else {
+            User user = new User(username.getText(), name.getText(), dob.getValue());
+            Comm comm = new Comm(email.getText(), mobile.getText());
+            Security security = new Security(password.getText(), passcode.getText());
+            Address address = new Address(address1.getText(), address2.getText(), postal.getText(), city.getText(), state.getText(), country.getText());
+            Application.client = new Client(user, security, comm, address);
+            int id = Application.database.addNewUser(Application.client);
+            Alert alert;
+            if(id != -1) {
+                alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Accounts!");
+                alert.setContentText("Account Created!");
+            } else {
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Accounts!");
+                alert.setContentText("Unable to Create Account!");
+            }
+            alert.show();
+        }
     }
 }
