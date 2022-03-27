bc3180b7ba100d43a6021a40116723bce526032a
laava <laavart11@gmail.com>
2022-03-27 18:46:28 +0530

    Commit

--- src/main/java/com/psloba/megabucks/Application.java
+++ src/main/java/com/psloba/megabucks/Application.java
@@ -10,7 +10,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 public class Application extends javafx.application.Application {
 
