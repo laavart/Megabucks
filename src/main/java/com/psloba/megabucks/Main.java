218aafe35f48eb4a4e02a33e873eddffd8547057
laava <laavart11@gmail.com>
2022-04-02 07:50:13 +0530

    Commit

--- src/main/java/com/psloba/megabucks/Main.java
+++ src/main/java/com/psloba/megabucks/Main.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+
 import citra.util.Pair;
 
 import javafx.collections.FXCollections;
