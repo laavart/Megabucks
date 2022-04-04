3b965424bff8c52832c42a839b86b3fb8bb268f4
laava <laavart11@gmail.com>
2022-04-04 08:17:42 +0530

    Commit

--- src/main/java/com/psloba/megabucks/Create.java
+++ src/main/java/com/psloba/megabucks/Create.java
@@ -149,83 +149,88 @@ public class Create {
     }
 
     @FXML
-    private void onSubmit() throws IOException {
+    private void onSubmit() {
         boolean check = true;
-        if(name.getText().equals("")) {
-            name.setPromptText("FIELD EMPTY!");
-            check = false;
+        {
+            if (name.getText().equals("")) {
+                name.setPromptText("FIELD EMPTY!");
+                check = false;
+            }
+            if (dob.getValue() == null) {
+                dob.setPromptText("FIELD EMPTY!");
+                check = false;
+            }
+            if (email.getText().equals("")) {
+                email.setPromptText("FIELD EMPTY!");
+                check = false;
+            }
+            if (mobile.getText().equals("")) {
+                mobile.setPromptText("FIELD EMPTY!");
+                check = false;
+            }
+            if (username.getText().equals("")) {
+                username.setPromptText("FIELD EMPTY!");
+                check = false;
+            }
+            if (password.getText().equals("")) {
+                password.setPromptText("FIELD EMPTY!");
+                check = false;
+            }
+            if (repassword.getText().equals("")) {
+                repassword.setPromptText("FIELD EMPTY!");
+                check = false;
+            }
+            if (passcode.getText().equals("")) {
+                passcode.setPromptText("FIELD EMPTY!");
+                check = false;
+            }
+            if (address1.getText().equals("")) {
+                address1.setPromptText("FIELD EMPTY!");
+                check = false;
+            }
+            if (postal.getText().equals("")) {
+                postal.setPromptText("FIELD EMPTY!");
+                check = false;
+            }
+            if (city.getText().equals("")) {
+                city.setPromptText("FIELD EMPTY!");
+                check = false;
+            }
+            if (state.getText().equals("")) {
+                state.setPromptText("FIELD EMPTY!");
+                check = false;
+            }
+            if (country.getText().equals("")) {
+                country.setPromptText("FIELD EMPTY!");
+                check = false;
+            }
+            if (!messagebox.getText().equals("Messages:\n")) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("MegaBucks");
+                alert.setContentText("Resolve all the Problems First!");
+                alert.show();
+                check = false;
+            }
         }
-        if(dob.getValue() == null){
-            dob.setPromptText("FIELD EMPTY!");
-            check = false;
-        }
-        if(email.getText().equals("")) {
-            email.setPromptText("FIELD EMPTY!");
-            check = false;
-        }
-        if(mobile.getText().equals("")) {
-            mobile.setPromptText("FIELD EMPTY!");
-            check = false;
-        }
-        if(username.getText().equals("")) {
-            username.setPromptText("FIELD EMPTY!");
-            check = false;
-        }
-        if(password.getText().equals("")) {
-            password.setPromptText("FIELD EMPTY!");
-            check = false;
-        }
-        if(repassword.getText().equals("")) {
-            repassword.setPromptText("FIELD EMPTY!");
-            check = false;
-        }
-        if(passcode.getText().equals("")) {
-            passcode.setPromptText("FIELD EMPTY!");
-            check = false;
-        }
-        if(address1.getText().equals("")) {
-            address1.setPromptText("FIELD EMPTY!");
-            check = false;
-        }
-        if(postal.getText().equals("")) {
-            postal.setPromptText("FIELD EMPTY!");
-            check = false;
-        }
-        if(city.getText().equals("")) {
-            city.setPromptText("FIELD EMPTY!");
-            check = false;
-        }
-        if(state.getText().equals("")) {
-            state.setPromptText("FIELD EMPTY!");
-            check = false;
-        }
-        if(country.getText().equals("")) {
-            country.setPromptText("FIELD EMPTY!");
-            check = false;
-        }
-        if(!messagebox.getText().equals("Messages:\n")) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("MegaBucks");
-            alert.setContentText("Resolve all the Problems First!");
-            alert.show();
-            check = false;
-        }
+
         if(check) {
+
             User user = new User(username.getText(), name.getText(), dob.getValue());
             Comm comm = new Comm(email.getText(), mobile.getText());
             Security security = new Security(password.getText(), passcode.getText());
             Address address = new Address(address1.getText(), address2.getText(), postal.getText(), city.getText(), state.getText(), country.getText());
             Client client = new Client(user, security, comm, address);
-            int id = AppData.database.addNewUser(client);
+
             Alert alert;
+            int id = AppData.database.addNewUser(client);
+
             if(id != -1) {
-                alert = new Alert(Alert.AlertType.CONFIRMATION);
-                alert.setTitle("MegaBucks");
-                alert.setContentText("Account Created!");
+                System.out.println("Primary Stage registration completed of player_" + id);
                 try {
                     AppData.database.executeUpdate("start transaction;");
 
                     AppData.database.executeUpdate("delete from player_data where id = " + id + ";");
+
                     AppData.database.executeUpdate(
                             "insert into player_data value(" +
                                     id + "," +
@@ -234,27 +239,30 @@ public class Create {
                                     ");"
                     );
 
-                    if(AppData.database.searchTable("player_"+id)) {
-                        AppData.database.executeUpdate("drop table player_" + id + ";");
+                    if(AppData.database.searchTable("player_"+id)) AppData.database.executeUpdate("delete from player_" + id + ";");
+                    else {
+                        AppData.database.executeUpdate(
+                                "create table player_" + id + "(" +
+                                        "date_time datetime," +
+                                        "sender int," +
+                                        "receiver int," +
+                                        "message varchar(250)" +
+                                        ");"
+                        );
                     }
-                    AppData.database.executeUpdate(
-                            "create table player_" + id + "(" +
-                                    "date_time datetime," +
-                                    "sender int," +
-                                    "receiver int," +
-                                    "message varchar(250)" +
-                                    ");"
-                    );
 
                     AppData.database.executeUpdate("commit;");
 
+                    alert = new Alert(Alert.AlertType.CONFIRMATION);
+                    alert.setTitle("MegaBucks");
+                    alert.setContentText("Account Created!");
                     alert.show();
+
+                    AppData.getScene("login");
                 }
-                catch (SQLException e) {
+                catch (SQLException | IOException e) {
                     e.printStackTrace();
                 }
-
-                AppData.getScene("login");
             }
             else {
                 alert = new Alert(Alert.AlertType.ERROR);
@@ -262,6 +270,6 @@ public class Create {
                 alert.setContentText("Unable to Create Account!");
                 alert.show();
             }
-        }
+        } else System.out.println("Receiving False");
     }
 }
